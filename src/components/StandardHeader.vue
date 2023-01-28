3a201b7c76b7e477ba718b6d5d45e7bd4532ef6e
Arthur Emiliano <arthur.emiliano14@gmail.com>
2023-01-28 07:49:42 -0300

    página de reservas criada em vue

---
+++ src/components/StandardHeader.vue
@@ -0,0 +1,100 @@
+<template>
+    <header>
+        <section>
+            <div>
+                <div class="userBar">
+                    <a id="login">Login</a>
+                    <a id="logout">Sair</a>
+                </div>
+                <span class="mainHeader">
+                    <a href="./index.html">
+                        <img src="../../public/images/logo_1800.png" alt="" />
+                    </a>
+                    <nav class="navbar menu">
+                        <a href="./index.html">Início</a>
+                        <a href="./pages/sobre.html">Nosso Hotel</a>
+                        <a href="./pages/acomodacoes.html">Acomodações</a>
+                        <a href="./pages/reservas.html">Reservas</a>
+                        <a href="#contato">Contato</a>
+                    </nav>
+                </span>
+                <div class="nomeLogo">
+                    <h1>Hotel 1800</h1>
+                </div>
+            </div>
+        </section>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "StandardHeader",
+};
+</script>
+
+<style scoped>
+header {
+    display: flex;
+    background-color: #ede5e3;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    background-attachment: fixed;
+    border-top: 2px solid rgba(192, 80, 25, 0.5019607843);
+    border-bottom: 2px solid rgba(192, 80, 25, 0.5019607843);
+    background-image: url("../../public/images/imagemHeader.jpg");
+    background-attachment: fixed;
+}
+header section {
+    width: 100%;
+}
+
+.userBar {
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    height: 1rem;
+    padding: 20px 40px 20px 20px;
+    gap: 0.3rem;
+    color: #fff3f1;
+}
+.userBar a {
+    display: flex;
+    min-width: 60px;
+    padding: 0.5rem;
+    justify-content: center;
+    background-color: #ad9479;
+    border-radius: 0.5rem;
+}
+.userBar a:hover {
+    font-weight: bold;
+    background-color: #82694e;
+}
+
+.mainHeader {
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+}
+.mainHeader img {
+    max-height: 8rem;
+    max-width: 8rem;
+}
+
+.navbar {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: flex-end;
+    gap: 0.3rem;
+}
+.navbar a {
+    padding: 0.5rem;
+    background-color: #ad9479;
+    border-radius: 0.5rem;
+    font-size: 1rem;
+}
+.navbar a:hover {
+    font-weight: bold;
+    background-color: #82694e;
+}
+</style>
---
+++ src/pages/reservas/ReservasPages.vue
@@ -0,0 +1,111 @@
+<template>
+    <div>
+        <StandardHeader />
+        <FacaSuaReserva />
+        <div class="produtos">
+            <div><ProdutosReserva class="ab" /></div>
+            <div class="resumoReserva"><ResumoReserva /></div>
+        </div>
+        <div class="Algo">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
+            iusto, maxime eveniet laboriosam eos beatae molestias? Quidem
+            distinctio earum ratione vel assumenda commodi veniam sequi
+            aspernatur explicabo animi? Velit, corporis iste accusantium in
+            tenetur, laudantium, maiores molestiae autem similique eligendi
+            eaque deserunt? Numquam atque ex voluptatum, delectus beatae fuga
+            corporis dolorem ad quasi quis odio. Vero deserunt dolorem qui dicta
+            hic iste minima fugiat ad. Dolore, qui. Ipsa nobis impedit
+            dignissimos maiores odio perferendis commodi veritatis repellat
+            possimus expedita id, excepturi adipisci! Eos sint voluptas minus
+            rem. Cumque facere nostrum quia rerum eveniet maiores labore iste
+            velit magnam? Exercitationem, assumenda.
+        </div>
+    </div>
+</template>
+
+<script>
+import StandardHeader from "../../components/StandardHeader.vue";
+import FacaSuaReserva from "../../components/FacaSuaReserva.vue";
+import ProdutosReserva from "../../components/ProdutosReserva.vue";
+import ResumoReserva from "../../components/ResumoReserva.vue";
+
+export default {
+    name: "ReservasPage",
+    data() {},
+    components: {
+        StandardHeader,
+        FacaSuaReserva,
+        ProdutosReserva,
+        ResumoReserva,
+    },
+};
+</script>
+
+<style>
+@charset "UTF-8";
+* {
+    font-family: "Baskerville";
+    box-sizing: border-box;
+    line-height: 1.5rem;
+}
+
+/*.......MIDIA QUERIES.......*/
+@font-face {
+    font-family: Baskerville;
+    src: url(../../../public/fonts/Libre_Baskerville/LibreBaskerville-Regular.ttf);
+}
+html {
+    scroll-behavior: smooth;
+}
+
+body {
+    color: #82694e;
+    background-color: #fff3f1;
+    font-family: "Baskerville";
+}
+
+a {
+    all: unset;
+    cursor: pointer;
+    transition: background-color 0.6s ease;
+}
+
+.menu a {
+    color: #fff3f1;
+}
+
+h1 {
+    font-size: 3rem;
+    font-style: bold;
+    padding: 0vh 0vh 0vh 24vh;
+    color: #e9cb6b;
+}
+
+h2 {
+    margin: 0;
+    font-size: 1.5rem;
+    line-height: 5rem;
+    text-align: center;
+    font-family: "Baskerville";
+}
+
+.principal {
+    margin: 1px 30px;
+}
+
+.nomeLogo {
+    display: flex;
+    padding-left: 0;
+}
+
+.produtos {
+    display: grid;
+    margin: 0 0 0 2.5rem;
+    grid-template-columns: 75% 25%;
+}
+
+.Algo {
+    border: 1px black solid;
+    height: 100rem;
+}
+</style>
