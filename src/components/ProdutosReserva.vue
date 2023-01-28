7bddfbfaba340e7e14d37fc6bfa7f8548aaa4e3c
Arthur Emiliano <116170868+Arthuremiliano@users.noreply.github.com>
2023-01-28 07:57:14 -0300

    página de reservas criada em vue (#68)

---
+++ src/components/ProdutosReserva.vue
@@ -0,0 +1,96 @@
+<template>
+    <div class="postArea" v-for="suite in suites" :key="suite.id">
+        <img :src="suite.images" :alt="suite.name" />
+        <div class="infoQuarto">
+            <h2>{{ suite.name }}</h2>
+            <p class="descQuartoTxt">{{ suite.description }}</p>
+            <h3>R$ {{ suite.price.toFixed(2) }}</h3>
+            <label>
+                <p class="qntQuartoTxt">Quantidade de quartos</p>
+                <select
+                    :id="'select' + suite.id"
+                    :name="'select' + suite.id"
+                    class="selectSuite"
+                >
+                    <option value="0">0</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                </select>
+            </label>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: "ProdutosReservas",
+    data() {
+        return {
+            suites: [
+                {
+                    id: 1,
+                    name: "Suíte Família",
+                    description:
+                        "Nossas suítes possuem conforto e luxo, com itens como televisão, ar condicionado, lençóis 100% algodão egípcio, internet gratuita, mini bar e café da manhã na cama. Desfrute de todos esses itens e muito mais em sua estadia conosco.",
+                    price: 150,
+                    images: "/images/acomodacao1.jpg",
+                },
+                {
+                    id: 2,
+                    name: "Suíte Master",
+                    description:
+                        "Os quartos do hotel oferecem uma variedade de comodidades, incluindo uma banheira com chuveiro, uma máquina de café com cápsulas, uma sala de estar, flores naturais, internet gratuita, roupão de banho e muito mais. Cada detalhe foi cuidadosamente planejado para garantir o conforto e o bem-estar dos hóspedes.",
+                    price: 250,
+                    images: "/images/acomodacao2.jpg",
+                },
+                {
+                    id: 3,
+                    name: "Suite Premium",
+                    description:
+                        "O Terraço com Jacuzzi é o destaque do hotel, oferecendo uma vista deslumbrante e um ambiente relaxante para os hóspedes. Além disso, o pátio conta com cadeiras e mesas para desfrutar do ar livre. As suítes possuem banheira e ar condicionado para maior conforto, além de lençóis 100% algodão egípcio. Estes detalhes tornam a estadia ainda mais agradável e luxuosa.",
+                    price: 350,
+                    images: "/images/acomodacao3.jpg",
+                },
+            ],
+        };
+    },
+};
+</script>
+<style scoped>
+.postArea {
+    border: 1px solid #ad9479;
+    display: flex;
+    margin: 1rem 0 1rem 0;
+    border-radius: 15px;
+}
+
+.postArea img {
+    max-width: 40%;
+    border-radius: 15px;
+    margin: 10px;
+}
+
+.infoQuarto {
+    margin: 0;
+}
+
+.descQuartoTxt {
+    margin: 0 10px 0 0;
+    text-align: justify;
+}
+
+.qntQuartoTxt {
+    display: inline-block;
+    margin: 0 10px 10px 0px;
+}
+
+.selectSuite {
+    border: none;
+    background: none;
+    border-bottom-color: #ad9479;
+    color: #6f5f4e;
+    border-bottom-width: 1px;
+    border-bottom-style: solid;
+}
+</style>
---
+++ src/components/ResumoReserva.vue
@@ -0,0 +1,82 @@
+<template>
+    <div class="fullDiv">
+        <div class="titleDiv">
+            <h2 class="totalTitle">Total</h2>
+            <h2 class="valorTitle">R$ 1200</h2>
+            <Button_1 class="button" />
+        </div>
+        <div class="resultsDiv">
+            <h4>Check-in: {{ checkIn }}</h4>
+            <h4>Check-out: {{ checkOut }}</h4>
+            <h4>Diárias: {{ diarias }}</h4>
+            <h4>Adultos: {{ qtdAdultos }}</h4>
+            <h4>Crianças: {{ qtdCrianca }}</h4>
+        </div>
+    </div>
+</template>
+<script>
+import Button_1 from "./Button_1.vue";
+
+export default {
+    name: "ResumoReserva",
+    data() {
+        return {
+            checkIn: "",
+            checkOut: "",
+            diarias: "",
+            qtdAdultos: "",
+            qtdCrianca: "",
+        };
+    },
+    components: {
+        Button_1,
+    },
+    
+};
+</script>
+<style scoped>
+.fullDiv {
+    position: sticky;
+    top: 2%;
+    margin: 1rem 2.5rem 0 15px;
+}
+
+.infoReserva {
+    border: 1px solid #6f5f4e;
+    border-radius: 15px;
+    margin: 1rem 2.5rem 0 15px;
+}
+
+.titleDiv {
+    background-color: #3f362d;
+    border: 1px solid #3f362d;
+    border-radius: 15px;
+    border-bottom-left-radius: 0px;
+    border-bottom-right-radius: 0px;
+}
+
+.totalTitle,
+.valorTitle {
+    color: #fff3f1;
+}
+
+.valorTitle {
+    margin: -40px 0 0 0;
+}
+
+.totalTitle {
+    margin: -10px 0 0 0;
+}
+
+.button {
+    margin: -10px 0 20px 0;
+}
+
+.resultsDiv {
+    background-color: #fff3f1;
+    border: 1px solid #3f362d;
+    border-radius: 15px;
+    border-top-left-radius: 0px;
+    border-top-right-radius: 0px;
+}
+</style>
